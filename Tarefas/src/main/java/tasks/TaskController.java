31ff52d695bc157d783250b333af7105853a3ac9
Lprinhoo <57637917+Lprinhoo@users.noreply.github.com>
2024-08-05 20:01:06 -0300

    Add files via upload

---
+++ Tarefas/src/main/java/tasks/TaskController.java
@@ -0,0 +1,133 @@
+package tasks;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("tasks")
+public class TaskController {
+
+    @PostMapping
+    public CreateTaskOutput post(@RequestBody CreateTaskInput in) {
+
+        return new CreateTaskOutput(
+                1L,
+                in.title(),
+                in.description(),
+                "a fazer",
+                in.due_date(),
+                in.assigned_id(),
+                in.project_id(),
+                LocalDateTime.now(),
+                LocalDateTime.now()
+        );
+
+    }
+
+    public record CreateTaskInput(
+            String title,
+            String description,
+            String status,
+            LocalDateTime due_date,
+            Long assigned_id,
+            Long project_id
+    ) {
+    }
+
+    public record CreateTaskOutput(
+            Long id,
+            String title,
+            String description,
+            String status,
+            LocalDateTime due_date,
+            Long assigned_id,
+            Long project_id,
+            LocalDateTime created_at,
+            LocalDateTime updated_at
+    ) {
+    }
+
+    @GetMapping("{task_id}")
+    public GetTaskByIdOutput getById(@PathVariable Long task_id) {
+
+        return new GetTaskByIdOutput(
+                task_id,
+                "Implementar autenticação",
+                "Implementar sistema de login e registro de usuários. ",
+                "a fazer",
+                LocalDate.of(2024, 7, 20),
+                1L,
+                1L,
+                LocalDateTime.of(2024, 7, 17, 0, 0, 0),
+                LocalDateTime.of(2024, 7, 17, 0, 0, 0)
+        );
+
+    }
+
+    public record GetTaskByIdOutput(
+            Long id,
+            String title,
+            String description,
+            String status,
+            LocalDate due_date,
+            Long assigned_id,
+            Long project_id,
+            LocalDateTime created_at,
+            LocalDateTime updated_at
+    ) {
+
+    }
+
+
+    @PutMapping("{task_id}")
+    public UpdateTaskOutput update(@PathVariable Long task_id, @RequestBody UpdateTaskInput in) {
+
+        return new UpdateTaskOutput(
+                task_id,
+                in.title(),
+                "Implementar sistema de login e registro de usuários. ",
+                in.status(),
+                LocalDate.of(2024, 7, 20),
+                1L,
+                1L,
+                LocalDateTime.of(2024, 7, 17, 0, 0, 0),
+                LocalDateTime.now()
+        );
+
+    }
+
+    public record UpdateTaskInput(
+            String title,
+            String status
+    ) {
+    }
+
+    public record UpdateTaskOutput(
+            Long id,
+            String title,
+            String description,
+            String status,
+            LocalDate due_date,
+            Long assigned_id,
+            Long project_id,
+            LocalDateTime created_at,
+            LocalDateTime updated_at
+    ) {
+    }
+
+    @DeleteMapping("{task_id}")
+    public DeleteTaskOutput delete(@PathVariable Long task_id) {
+
+        return new DeleteTaskOutput(
+                "Task deleted successfully"
+        );
+
+    }
+
+    public record DeleteTaskOutput(
+            String message
+    ) {
+    }
+}
---
+++ Tarefas/src/main/java/tasks/UserController.java
@@ -0,0 +1,110 @@
+package tasks;
+
+import org.springframework.cglib.core.Local;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.CryptoPrimitive;
+import java.time.LocalDateTime;
+
+import static java.lang.System.in;
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+
+
+    @PostMapping
+    public CreateUserOutput post(@RequestBody CreateUserInput in) {
+        return new CreateUserOutput(
+                1L,
+                in.name(),
+                in.email(),
+                SHA256.execute(in.password()),
+                LocalDateTime.now(),
+                LocalDateTime.now()
+        );
+    }
+
+    public record CreateUserInput(
+            String name,
+            String email,
+            String password
+    ) {
+    }
+
+    public record CreateUserOutput(
+            Long id,
+            String name,
+            String email,
+            String password,
+            LocalDateTime created_at,
+            LocalDateTime updated_at
+    ) {
+    }
+
+    @GetMapping("{id}")
+    public GetUserByIdOutput getById(@PathVariable Long id) {
+
+        String password = "123456";
+        String cryptoPassword = SHA256.execute(password);
+        return new GetUserByIdOutput(
+                id,
+                "John Doe",
+                "john@example.com",
+                cryptoPassword,
+                LocalDateTime.of(2024, 7, 17, 0, 0, 0),
+                LocalDateTime.of(2024, 7, 17, 0, 0, 0)
+        );
+    }
+
+    public record GetUserByIdOutput(
+            Long id,
+            String name,
+            String email,
+            String password,
+            LocalDateTime created_at,
+            LocalDateTime updated_at
+    ) {
+    }
+
+    @PutMapping("{id}")
+    public UpdateUserOutput update(@PathVariable Long id, @RequestBody UpdateUserInput in) {
+        return new UpdateUserOutput(
+                id,
+                in.name(),
+                in.email(),
+                LocalDateTime.of(2024, 7, 17, 0, 0, 0),
+                LocalDateTime.of(2024, 7, 17, 0, 0, 0)
+        );
+    }
+
+    public record UpdateUserInput(
+            String name,
+            String email
+    ) {
+    }
+
+    public record UpdateUserOutput(
+            Long id,
+            String name,
+            String email,
+            LocalDateTime created_at,
+            LocalDateTime updated_at
+    ) {
+    }
+
+    @DeleteMapping("{id}")
+    public UserController.DeleteUserOutput delete(@PathVariable Long id) {
+
+        return new UserController.DeleteUserOutput(
+                "User deleted successfully"
+        );
+
+    }
+
+    public record DeleteUserOutput(
+            String message
+    ) {
+    }
+
+}
